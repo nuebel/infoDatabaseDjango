a1b3b9a4ba2f8c78e27cc9ef85d17e37c0fa888f
Nic Uebel <nic@nicuebel.com>
2020-03-16 09:45:46 -0400

    Added search and sort

---
+++ student/static/student/js/directory.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+$(".sorter").click( function(event) {
+	var sort_by;
+	if ( $(this).attr('id') == "sorter_lastname") sort_by = "last_name";
+	else if ($(this).attr('id') == "sorter_firstname") sort_by = "first_name";
+	else if ($(this).attr('id') == "sorter_schoolyear") sort_by = "school_year";
+	else if ($(this).attr('id') == "sorter_color") sort_by = "color";
+	else if ($(this).attr('id') == "sorter_address") sort_by = "address";
+	console.log(`Sorting results by ${sort_by}`);
+
+	var search_field = "last_name";
+	var search_text = "";
+
+	$.ajax({
+		context: this,
+		type : 'GET',
+		url : '/student/ajax_searchsort',
+		data : "search_field=" + search_field + "&search_text=" +  search_text + "&sort_by=" + sort_by
+	})
+		.done(function( data ) {
+			//Replace the directory div with the Ajax data
+			$('#directory').html(data);
+
+			//Make the current sort by link unclickable and activate the one that was inactive
+			$(".current").removeClass("current");
+			$(this).addClass("current");
+	})
+		.fail(function() {
+			console.log('Ajax get failed');
+	});
+});
+
+});
